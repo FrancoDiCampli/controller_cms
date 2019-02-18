4b8e9b6dd2dcfdd0ad52c9da48c0a20d764515a8
francodicampli <francodicampli@gmail.com>
2019-02-18 10:10:41 -0300

    testig github

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Plantillas
                 </div>
 
                 <div class="links">
